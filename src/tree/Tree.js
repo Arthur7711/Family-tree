99d062bc1199727688f2ed1b2b61f39228a3a83b
Arthur Ghazaryan <newartroll11@gmail.com>
2021-10-02 17:39:59 +0400

    feat:(classnames)

--- src/tree/Tree.js
+++ src/tree/Tree.js
@@ -2,9 +2,45 @@ import React from "react";
 import Person from "./components/Person";
 
 const Tree = () => {
+  const dataTree = [
+    {
+      id: "p1",
+      gender: "male",
+      parents: [],
+      children: [],
+      spouse: [],
+      name: "someName",
+    },
+    {
+      id: "p2",
+      gender: "female",
+      parents: [],
+      children: [],
+      spouse: [],
+      name: "someName",
+    },
+    {
+      id: "p3",
+      gender: "female",
+      parents: [],
+      children: [],
+      spouse: [],
+      name: "someName",
+    },
+    {
+      id: "p4",
+      gender: "male",
+      parents: [],
+      children: [],
+      spouse: [],
+      name: "someName",
+    },
+  ];
   return (
     <div>
-      <Person WIDTH="70px" HEIGHT="80px" />
+      {dataTree.map((person) => (
+        <Person COLOR={person.gender} WIDTH="70px" HEIGHT="80px" />
+      ))}
     </div>
   );
 };
--- src/tree/components/Person.js
+++ src/tree/components/Person.js
@@ -1,7 +1,8 @@
 import React from "react";
 import styles from "./Person.module.css";
+import classNames from "classnames";
 
-const Person = ({ WIDTH, HEIGHT }) => {
+const Person = ({ WIDTH, HEIGHT,COLOR }) => {
   return (
     // <div className={styles.pers}>
       <div className={styles.inner} style={{ width: WIDTH, height: HEIGHT }} />
