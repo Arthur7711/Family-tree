5b3d3737132508aa0dbac9850c24275f85fa2d2b
Arthur <newartroll11@gmail.com>
2021-10-06 18:10:20 +0400

    fix:(not using areas)

--- src/tree/components/oneLine/OneLine.js
+++ src/tree/components/oneLine/OneLine.js
@@ -1,7 +1,7 @@
 import styles from "./OneLine.module.css";
 import classNames from "classnames";
 import Xarrow from "react-xarrows";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const Oneline = ({ id, name, gender, spouse, children, parentId, childs }) => {
   const [x, setX] = useState(100);
@@ -12,14 +12,11 @@ const Oneline = ({ id, name, gender, spouse, children, parentId, childs }) => {
     setX(x + 100);
     setY(y + 100);
   }
-  // useEffect(() => {
-  //   getNewPosition();
-  // }, [x, y]);
+
   return (
     <div style={{ position: "relative" }}>
       <div
         id={id}
-        // style={{ position: "relative" }}
         className={classNames(styles.inner, styles[gender])}
         title={`id_${id}, spouse_${spouse}, childrens_${childs}`}
       >
