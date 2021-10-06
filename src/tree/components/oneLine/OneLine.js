2f2e51cca4f196fbe6007fbc8f34b33fb56f4281
Arthur <newartroll11@gmail.com>
2021-10-06 18:00:08 +0400

    feat:(inv styles and posting feat)

--- src/tree/components/oneLine/OneLine.js
+++ src/tree/components/oneLine/OneLine.js
@@ -1,14 +1,25 @@
 import styles from "./OneLine.module.css";
 import classNames from "classnames";
 import Xarrow from "react-xarrows";
+import { useEffect, useState } from "react";
 
 const Oneline = ({ id, name, gender, spouse, children, parentId, childs }) => {
+  const [x, setX] = useState(100);
+  const [y, setY] = useState(100);
   const hasChildren = children && children.length;
 
+  function getNewPosition() {
+    setX(x + 100);
+    setY(y + 100);
+  }
+  // useEffect(() => {
+  //   getNewPosition();
+  // }, [x, y]);
   return (
-    <>
+    <div style={{ position: "relative" }}>
       <div
         id={id}
+        // style={{ position: "relative" }}
         className={classNames(styles.inner, styles[gender])}
         title={`id_${id}, spouse_${spouse}, childrens_${childs}`}
       >
@@ -16,9 +27,21 @@ const Oneline = ({ id, name, gender, spouse, children, parentId, childs }) => {
         {spouse}
       </div>
       {hasChildren
-        ? children.map((item) => (
-            <div style={{ padding: "30px" }}>
-              <Oneline key={item.id} {...item} />
+        ? children.map((item, key) => (
+            <div
+              className="test1"
+              style={{
+                padding: "30px",
+                position: "absolute",
+                top: y,
+                left: 100 * key,
+              }}
+            >
+              <Oneline
+                key={item.id}
+                {...item}
+                getNewPosition={getNewPosition}
+              />
               <Xarrow
                 start={item.parentId} //can be react ref
                 end={item.id} //or an id
@@ -26,7 +49,7 @@ const Oneline = ({ id, name, gender, spouse, children, parentId, childs }) => {
             </div>
           ))
         : null}
-    </>
+    </div>
   );
 };
 
